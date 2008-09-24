6531809b56ca0b5a0fb2ef7e49f4a43b783d676d
nosklo <nosklo@3bfa2988-8a4f-11dd-8937-b3a01dae0fbe>
2008-09-24 20:13:02 +0000

    Refactored property typechecking code

--- mpylayer/mpylayer_control.py
+++ mpylayer/mpylayer_control.py
@@ -33,27 +33,57 @@ class MPlayerControl(object):
         self._assert_mplayer()
         if cmd == 'quit':
             return self._mp.communicate('quit\n')[0]
-        args = ' '.join(self._process_args(cmd, self._cmds[cmd], *args))
+        if cmd != 'set_property': # set_property must give converted values
+            args = self._process_args(cmd, self._cmds[cmd], *args)
+        args = ' '.join(args)
         self._flush()
         self._mp.stdin.write('pausing_keep %s %s\n' % (cmd, args))
         if cmd.startswith('get_'):
-            time.sleep(0.1) # wait a little bit for the result, since it is a get
+            time.sleep(0.1) # wait little bit for the result, since it is a get
             return self._get_result()
 
     def __getitem__(self, item):
-        return self.get_property(item)
+        """Retrieves a property"""
+        value = self.get_property(item)
+        proptype = _property_table[item]['type']
+        if proptype == 'flag':
+            value = value == 'yes'
+        elif proptype in ('float', 'int', 'pos', 'time'):
+            try:
+                value = float(value)
+            except ValueError:
+                value = None
+        return value
 
     def __setitem__(self, item, value):
-        self.set_property(item, value)
+        """Sets a property"""
+        prop = _property_table[item]
+        min = prop['min']
+        max = prop['max']
+        proptype = prop['type']        
+        if proptype == 'flag':
+            value = int(bool(value))
+        elif proptype in ('int', 'float', 'pos', 'time'):
+            value = float(value)
+        else:
+            value = repr(value)
+        if min is not None and value < min:
+            raise ValueError("%s value can't be less than %s" %
+                             (item, min))
+        if max is not None and value > max:
+            raise ValueError("%s value can't be more than %s" %
+                             (item, max))
+        self.set_property(item, str(value))
 
     def __new__(cls, mplayer_path='mplayer', *args, **kwds):
-        # create list of commands:
+        # create the list of commands from mplayer:
         cmdlist = Popen([mplayer_path, '-really-quiet', '-input', 'cmdlist'],
                         stdout=PIPE, stderr=IGNORE).communicate()[0].splitlines()
         cmdlist = (line.split() for line in cmdlist)
         cls._cmds = dict((line[0], line[1:]) for line in cmdlist)
         _debug(cls._cmds)
-        # create methods and properties:
+
+        # aux functions to create methods and property getters and setters
         def _create_runcmd(_run_method, _the_cmd, doc=''):
             @functools.wraps(_run_method)
             def _runcmd(self, *args, **kwds):
@@ -61,38 +91,36 @@ class MPlayerControl(object):
             _runcmd.__doc__ = doc
             _runcmd.__name__ = _the_cmd
             return _runcmd
+       
         def _create_get(_the_prop):
             def _pget(self):
                 return self[_the_prop]
             return _pget
-        def _create_set(_the_prop, min, max, func):
+
+        def _create_set(_the_prop):
             def _pset(self, value):
-                if min is not None and value < min:
-                    raise ValueError("%s value can't be less than %s" %
-                                     (_the_prop, min))
-                if max is not None and value > max:
-                    raise ValueError("%s value can't be more than %s" %
-                                     (_the_prop, max))
-                self[_the_prop] = func(value)
+                self[_the_prop] = value
             return _pset
 
+        # Defining methods
         for cmd, arglist in cls._cmds.iteritems():
             if cmd.startswith('get_') and cmd != 'get_property':
                 prop = property(fget=functools.partial(cls.run_command, cmd=cmd),
-                                doc='%s command (readonly)' % cmd)
+                                doc='%s command\n(readonly)' % cmd)
                 setattr(cls, cmd[4:], prop)
             else:
-                setattr(cls, cmd, _create_runcmd(cls.run_command, cmd,
-                                                 doc='%s(%s)' %  (cmd,
-                                                                  ', '.join(arglist))))
+                doc = '%s(%s)' % (cmd, ', '.join(arglist))
+                setattr(cls, cmd, _create_runcmd(cls.run_command, cmd, doc=doc))
+        
+        # Defining properties
         for prop_name, propdict in _property_table.iteritems():
             pget = _create_get(prop_name)
             if propdict['set']:
-                pset = _create_set(prop_name, propdict['min'], propdict['max'],
-                                              propdict['function'])
+                pset = _create_set(prop_name)
             else:
                 pset = None
-            doc = ["%(name)s  (%(type)s)" % propdict]
+            # create a docstring:
+            doc = ["%(name)s <%(type)s>" % propdict]
             line = []
             if not propdict['set']:
                 line.append('(readonly)')
@@ -103,6 +131,7 @@ class MPlayerControl(object):
             if propdict['comment']:
                 doc.append(propdict['comment'])
             doc = '\n'.join(doc)
+            # create property
             prop = property(fget=pget, fset=pset, doc=doc)
             setattr(cls, prop_name, prop)
 
@@ -115,14 +144,12 @@ class MPlayerControl(object):
         """
         self._mp_path = mplayer_path
         self._buffer = deque()
-        # self._get_cmdlist()
         self._run_mplayer()
-        # self._create_methods()
 
     _cmdtype = {
                 'Integer': (int, '%d'),
                 'Float': (float, '%f'),
-                'String': (str, '%s'),
+                'String': (str, '%r'),
                  }
 
     def _process_args(self, cmd, arglist, *args):
@@ -137,7 +164,7 @@ class MPlayerControl(object):
                     raise TypeError('%s takes %r arguments - %s not given in %r' %
                                     (cmd, arglist, arg, args))
             arg_func, arg_mask = self._cmdtype[arg]
-            yield arg_mask % arg_func(rcvd_args.pop())
+            yield arg_mask % arg_func(rcvd_args.pop())        
 
     def _run_mplayer(self):
         self._mp = Popen([self._mp_path, '-slave', '-quiet', '-idle'],
@@ -179,13 +206,10 @@ class MPlayerControl(object):
             value = self._buffer.pop()
             if '=' in value:
                 foo, foo, value = value.partition('=')
-            try:
-                return float(value)
-            except ValueError:
-                return value.strip("'").strip()
+            return value.strip("'").strip()
         else:
             return ''
 
 if __name__ == '__main__':
     mp = MPlayerControl()
-    mp.loadfile('/home/nosklo/midia/musica/Trick_as_a_brick.mp3')
+    mp.loadfile('Trick_as_a_brick.mp3')
